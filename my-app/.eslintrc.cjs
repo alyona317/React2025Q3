b5acd1427fbd115d65233408fbb0db0ccbc988fe
alyona317 <oschepkova.alyona@yandex.ru>
2025-07-08 22:38:41 +0300

    feat: add components

---
+++ my-app/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'jsx-a11y', 'import'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {},
+};
---
+++ my-app/src/components/Search.tsx
@@ -0,0 +1,37 @@
+import React, {Component, ChangeEvent} from "react";
+
+interface Props {
+  onSearch: (pocemonName: string) => void
+}
+
+interface State {
+  inputValue: string;
+}
+
+export class Search extends Component<Props, State> {
+  state: State = {inputValue: ''}
+
+  handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  this.setState({ inputValue: e.target.value });
+  }
+  handleClick = () => {
+    this.props.onSearch(this.state.inputValue.trim().toLowerCase());
+  };
+
+  render() {
+    return (
+      <div style={{ marginBottom: 20 }}>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+          placeholder="Введите имя покемона"
+          style={{ padding: '0.5rem', width: 200 }}
+        />
+        <button onClick={this.handleClick} style={{ marginLeft: 10, padding: '0.5rem 1rem' }}>
+          Поиск
+        </button>
+      </div>
+    );
+  }
+}
